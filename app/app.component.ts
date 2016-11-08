c7fa831e85b19dd93f411166a0c0f215b97c1466
Amber Farrington <amberf0712@outlook.com>
2016-11-08 15:23:46 -0800

    fix syntax error

--- app/app.component.ts
+++ app/app.component.ts
@@ -126,6 +126,7 @@ export class AppComponent {
   public hideHigh = false;
   public hideUser = false;
   fire(row: number,col: number){
+    // TODO put logic to test return value of fire method and play correct sound (fire returns "sunk", "hit", or "miss")
     var x = this.myGame.fire(row,col);
     if(x === "miss") {
       this.audio.src = "../../resources/sounds/splash.wav";
@@ -139,8 +140,13 @@ export class AppComponent {
     }else if (this.myGame.gameCompleted === true) {
       this.audio.src = "../../resources/sounds/winner.mp3";
       this.audio.play();
-      this.win();
-    }
+    this.myGame.fire(row,col);
+    this.audio.src = "../../resources/sounds/torpedo.wav";
+    this.audio.play();
+}
+  // if (this.myGame.hitShip === 17) {
+  //     this.win();
+  //   }
   }
   win() {
   	if (this.data.login) {
@@ -190,7 +196,7 @@ export class AppComponent {
 	      .subscribe((res: Response) => {
 	        this.highScore = res.json();
 	  });
-	  
+
 	  this.http.request('/userscore')
 	      .subscribe((res: Response) => {
 	        this.userScore = res.json();
