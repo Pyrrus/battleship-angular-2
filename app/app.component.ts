0bd58a5515430fbc7b7bb0a5a637a06ef13732df
Yusuf Qedan and jontoler <yusuf+jontoler@>
2016-11-08 16:44:32 -0800

    tweak ai

--- app/app.component.ts
+++ app/app.component.ts
@@ -127,26 +127,13 @@ export class AppComponent {
   public hideUser = false;
   fire(row: number,col: number){
     // TODO put logic to test return value of fire method and play correct sound (fire returns "sunk", "hit", or "miss")
-    var x = this.myGame.fire(row,col);
-    if(x === "miss") {
-      this.audio.src = "../../resources/sounds/splash.mp3";
-      this.audio.play();
-    }else if(x === "sunk" && this.myGame.gameCompleted === false){
-      this.audio.src = "../../resources/sounds/explosion.mp3";
-      this.audio.play();
-    }else if(x === "hit"){
-      this.audio.src = "../../resources/sounds/torpedo.wav";
-      this.audio.play();
-    }else if (this.myGame.gameCompleted === true) {
-      this.audio.src = "../../resources/sounds/winner.mp3";
-      this.audio.play();
     this.myGame.fire(row,col);
     this.audio.src = "../../resources/sounds/torpedo.wav";
     this.audio.play();
-}
-  // if (this.myGame.hitShip === 17) {
-  //     this.win();
-  //   }
+
+  if (this.myGame.hitShip === 17) {
+      this.win();
+    }
   }
   win() {
   	if (this.data.login) {
@@ -165,11 +152,11 @@ export class AppComponent {
   	}
   }
   showHigh() {
-  	// this.http.request('/highscore')
-	  //     .subscribe((res: Response) => {
-	  //       this.highScore = res.json();
-	  // });
-  	// this.hideHigh = true;
+  	this.http.request('/highscore')
+	      .subscribe((res: Response) => {
+	        this.highScore = res.json();
+	  });
+  	this.hideHigh = true;
 
   }
   newGame(){
@@ -192,15 +179,15 @@ export class AppComponent {
 	        this.user = res.json();
 	  });
 
-	  // this.http.request('/highscore')
-	  //     .subscribe((res: Response) => {
-	  //       this.highScore = res.json();
-	  // });
-    //
-	  // this.http.request('/userscore')
-	  //     .subscribe((res: Response) => {
-	  //       this.userScore = res.json();
-	  // });
+	  this.http.request('/highscore')
+	      .subscribe((res: Response) => {
+	        this.highScore = res.json();
+	  });
+
+	  this.http.request('/userscore')
+	      .subscribe((res: Response) => {
+	        this.userScore = res.json();
+	  });
   }
 }
 
